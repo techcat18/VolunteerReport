923023ff3080ba76abcf81ac14c52e2773873fdc
Nikora Dmytro <dimanikora04@gmail.com>
2023-11-19 17:11:36 +0200

    Changed report model to match dto on server.

--- mobile/lib/models/report.dart
+++ mobile/lib/models/report.dart
@@ -1,7 +1,15 @@
 class Report {
-  Report(this.name, this.description, this.img);
+  Report(
+    this.name,
+    this.description,
+    this.img,
+    this.createdAt,
+    this.modifiedAt,
+  );
 
   String img;
   String name;
   String description;
+  DateTime createdAt;
+  DateTime modifiedAt;
 }
