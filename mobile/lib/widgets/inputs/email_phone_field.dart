087d511166cdfec04dcb21b98b21c7cf6228faba
Nikora Dmytro <dimanikora04@gmail.com>
2023-11-21 01:55:44 +0200

    Added validation of email or phone entered to email/phone input.

--- mobile/lib/widgets/inputs/email_phone_field.dart
+++ mobile/lib/widgets/inputs/email_phone_field.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
 class EmailPhoneField extends StatelessWidget {
   const EmailPhoneField({super.key});
+
+  bool _isEmail(String input) => EmailValidator.validate(input);
+  bool _isPhone(String input) =>
+      RegExp(r'^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$')
+          .hasMatch(input);
 
   @override
   Widget build(BuildContext context) {
@@ -10,6 +16,9 @@ class EmailPhoneField extends StatelessWidget {
       validator: (value) {
         if (value == null || value.trim().isEmpty) {
           return "This is a required field!";
+        }
+        if (!_isEmail(value) && !_isPhone(value)) {
+          return 'Must be a valid email or phone number!';
         }
 
         return null;
