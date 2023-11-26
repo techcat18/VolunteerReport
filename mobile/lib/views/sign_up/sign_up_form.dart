ac70ec6fba23d0597dc287106cdbcf3bc9134261
Nikora Dmytro <dimanikora04@gmail.com>
2023-11-27 01:41:54 +0200

    Created custom dropdown. It can act as input field to add new values.

--- mobile/lib/views/sign_up/sign_up_form.dart
+++ mobile/lib/views/sign_up/sign_up_form.dart
@@ -3,14 +3,19 @@ import 'package:reactive_forms/reactive_forms.dart';
 
 import 'package:mobile/helpers/phone_validator.dart';
 
-import 'package:mobile/widgets/inputs/dropdown_field.dart';
+import 'package:mobile/widgets/inputs/reactive_dropdown.dart';
 import 'package:mobile/widgets/inputs/reactive_email_phone.dart';
 import 'package:mobile/widgets/inputs/reactive_password.dart';
 import 'package:mobile/widgets/inputs/reactive_plain_text.dart';
 
-class SignUpForm extends StatelessWidget {
-  SignUpForm({super.key});
+class SignUpForm extends StatefulWidget {
+  const SignUpForm({super.key});
 
+  @override
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm> {
   List<Widget> _addSpacing(List<Widget> widgets) {
     return [
       for (Widget widget in widgets) ...[
@@ -34,42 +39,43 @@ class SignUpForm extends StatelessWidget {
         ],
       )
     ],
+    "organization": [Validators.required],
     "password": [Validators.required],
     "confirmPassword": [Validators.required],
   });
 
   @override
   Widget build(BuildContext context) {
-    return ReactiveFormBuilder(
-      form: () => form,
-      builder: (context, form, child) {
-        return Column(
-          children: _addSpacing(
-            [
-              const ReactivePlainText(
-                name: "name",
-                label: "Volunteer’s name",
-              ),
-              const ReactivePlainText(
-                name: "about",
-                label: "About",
-              ),
-              const ReactiveEmailPhone(name: "email"),
-              const DropDownField<String>(
-                label: "Organization",
-                items: [
-                  DropdownMenuEntry(value: "1", label: "Organization 1"),
-                  DropdownMenuEntry(value: "2", label: "Organization 2"),
-                  DropdownMenuEntry(value: "3", label: "Organization 3"),
-                  DropdownMenuEntry(value: "4", label: "Organization 4"),
-                ],
-              ),
-              const ReactivePassword(name: "password"),
-              const ReactivePassword(name: "confirmPassword"),
-            ],
-          ),
-        );
-      },
+    return ReactiveForm(
+      formGroup: form,
+      child: Column(
+        children: _addSpacing(
+          [
+            const ReactivePlainText(
+              name: "name",
+              label: "Volunteer’s name",
+            ),
+            const ReactivePlainText(
+              name: "about",
+              label: "About",
+            ),
+            const ReactiveEmailPhone(name: "email"),
+            const ReactiveDropdown(
+              name: "organization",
+              label: "Organization",
+              items: [
+                "Organization 1",
+                "Organization 2",
+                "Organization 3",
+                "Organization 4",
+                "Organization 5",
+              ],
+            ),
+            const ReactivePassword(name: "password"),
+            const ReactivePassword(name: "confirmPassword"),
+          ],
+        ),
+      ),
     );
   }
 }
