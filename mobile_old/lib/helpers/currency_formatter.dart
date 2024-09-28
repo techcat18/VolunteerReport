79f739ae6d60d745e0d2d26b7d08f37e386a4b56
Dmytro Nikora <dimanikora04@gmail.com>
2024-09-28 16:37:28 +0300

    Deleted mobile old.

--- mobile_old/lib/helpers/currency_formatter.dart
+++
@@ -1,6 +0,0 @@
-import 'package:intl/intl.dart';
-
-final currencyFormatter = NumberFormat.currency(
-  locale: "uk_UA",
-  symbol: "\u{20B4}",
-);
--- mobile_old/lib/models/report_category.dart
+++
@@ -1,9 +0,0 @@
-class ReportCategory {
-  ReportCategory({
-    required this.id,
-    required this.name,
-  });
-
-  String id;
-  String name;
-}
